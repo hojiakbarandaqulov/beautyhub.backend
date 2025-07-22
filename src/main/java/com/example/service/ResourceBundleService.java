ed96cedfe70a7b96f02cf38fbf3dc633abd9e2d0
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2025-07-22 23:36:12 +0500

    city search

--- src/main/java/com/example/service/ResourceBundleService.java
+++ src/main/java/com/example/service/ResourceBundleService.java
@@ -19,4 +19,8 @@ public class ResourceBundleService {
     public String getMessage(String code, LanguageEnum lang) {
         return resourceBundle.getMessage(code,null, new Locale(lang.name()));
     }
+
+    public String getMessage(String code, String lang) {
+        return resourceBundle.getMessage(code,null, new Locale(lang));
+    }
 }
